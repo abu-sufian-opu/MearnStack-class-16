2747947c0e9560c8337acf9272d266637bf2e105
abu-sufian-opu <sufian.jnu@gmail.com>
2022-02-04 23:03:34 +0600

    update

--- app.js
+++ app.js
@@ -49,19 +49,20 @@ const acc_header = document.querySelectorAll('.accordion-header');
 acc_header.forEach(items => {
     items.addEventListener('click', function(){
 
-        const curr_activeHeader = document.querySelector('.accordion-header.active');
+        acc_header.forEach( items => {
 
-        if( curr_activeHeader && curr_activeHeader !== items){
-            curr_activeHeader.classList.remove('active');
-            curr_activeHeader.nextElementSibling.style.height = 0;
-        }
+            if( items !== this){
+                items.classList.remove('active');
+                items.nextElementSibling.style.height = '0px';
+            }
+        });
         
         items.classList.toggle('active');
         
         if(items.classList.contains('active')){
             items.nextElementSibling.style.height = items.nextElementSibling.scrollHeight + 'px';
         }else{
-            items.nextElementSibling.style.height = '0';
+            items.nextElementSibling.style.height = '0px';
         }
     })
 });
