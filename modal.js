d8275fd72f1d40daff508731240dc78084156e8c
abu-sufian-opu <sufian.jnu@gmail.com>
2022-02-05 00:52:56 +0600

    Updated

--- modal.js
+++ modal.js
@@ -37,33 +37,3 @@ modal.addEventListener('click', function(e){
         modal.classList.remove('active');
     }
 });
-
-
-//custom-accordion
-
-//get elements
-
-const acc_header = document.querySelectorAll('.accordion-header');
-
-
-acc_header.forEach(items => {
-    items.addEventListener('click', function(){
-
-        acc_header.forEach( items => {
-
-            if( items !== this){
-                items.classList.remove('active');
-                items.nextElementSibling.style.height = '0px';
-            }
-        });
-        
-        items.classList.toggle('active');
-        
-        if(items.classList.contains('active')){
-            items.nextElementSibling.style.height = items.nextElementSibling.scrollHeight + 'px';
-        }else{
-            items.nextElementSibling.style.height = '0px';
-        }
-    })
-});
-
